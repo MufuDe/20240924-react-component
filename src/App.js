31bca9b6a950c3de2ce7bc13634030a30f100b83
chendechuan <chen0802@qq.com>
2024-09-24 16:07:31 +0800

    1. 条件渲染； 2. 渲染列表；

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import List from "./component/List";
+import PackingList from "./component/PackingList";
 import Profile from "./component/Profile";
 import TodoList from "./component/TodoList";
 
@@ -13,6 +15,14 @@ export default function Gallery() {
       <section>
         <h1>TodoList</h1>
         <TodoList />
+      </section>
+      <section>
+        <h1>PackingList</h1>
+        <PackingList />
+      </section>
+      <section>
+        <h1>List</h1>
+        <List />
       </section>
     </>
   );
---
+++ src/component/data.js
@@ -0,0 +1,38 @@
+export const people = [
+  {
+    id: 0,
+    name: "Creola Katherine Johnson",
+    profession: "mathematician",
+    accomplishment: "spaceflight calculations",
+    imageId: "01-scientist",
+  },
+  {
+    id: 1,
+    name: "Mario José Molina-Pasquel Henríquez",
+    profession: "chemist",
+    accomplishment: "discovery of Arctic ozone hole",
+    imageId: "02-scientist",
+  },
+  {
+    id: 2,
+    name: "Mohammad Abdus Salam",
+    profession: "physicist",
+    accomplishment: "electromagnetism theory",
+    imageId: "03-scientist",
+  },
+  {
+    id: 3,
+    name: "Percy Lavon Julian",
+    profession: "chemist",
+    accomplishment:
+      "pioneering cortisone drugs, steroids and birth control pills",
+    imageId: "04-scientist",
+  },
+  {
+    id: 4,
+    name: "Subrahmanyan Chandrasekhar",
+    profession: "astrophysicist",
+    accomplishment: "white dwarf star mass calculations",
+    imageId: "05-scientist",
+  },
+];
